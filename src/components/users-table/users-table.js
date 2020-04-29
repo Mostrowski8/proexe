3aca9365e0201da7c9e0281d7994e495bcee775d
Marek <mostrowski8@gmail.com>
2020-04-29 21:21:13 +0200

    add sorting

--- src/components/users-table/users-table.js
+++ src/components/users-table/users-table.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './users-table.css';
 
 const UsersTable = (props) => {
 console.log('usersprops', props)
+
+let [sortedUsers, setSortedUsers] = useState();
 
 const users = props.users.map(user => {
     return (
@@ -18,6 +20,29 @@ const users = props.users.map(user => {
     )
 })
 
+const sortUsers = () => {
+    let sorted = props.users.slice();
+    sorted.sort((a, b) => {
+        var textA = a.username.toUpperCase();
+        var textB = b.username.toUpperCase();
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+    const sortedUsers = sorted.map(user => {
+        return (
+            <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.address? user.address.city : ''}</td>
+                <td><button type="button" className="btn btn-warning" onClick={() => props.openAdd(user.id)}>Edit</button></td>
+                <td><button type="button" className="btn btn-danger" onClick={() => props.openDelete(user.id)}>Delete</button></td>
+            </tr>
+        )
+    })
+    setSortedUsers(sortedUsers);
+}
+
     return (
         <div className="card table-container ">
         <div className="add-user-panel">
@@ -30,6 +55,12 @@ const users = props.users.map(user => {
                 className="btn btn-primary add-btn" 
                 onClick={() => props.openAdd()}>
                     Add New
+                </button>
+                <button 
+                type="button" 
+                className="btn btn-primary add-btn" 
+                onClick={() => sortUsers()}>
+                    Sort
                 </button>
             </div>
         </div>
@@ -46,7 +77,7 @@ const users = props.users.map(user => {
                 </tr>
             </thead>
             <tbody>
-            {users}
+            {sortedUsers ? sortedUsers : users}
             </tbody>
         </table>
         </div>
