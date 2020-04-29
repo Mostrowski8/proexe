25de6acb1fa9eb0e82aec761066147f78707da7f
Marek <mostrowski8@gmail.com>
2020-04-29 19:41:49 +0200

    Updates

---
+++ precache-manifest.af9295a82d0ded78c88ff208b1d56f2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42139089b61fb0f5ce2bfa7850af23d9",
+    "url": "/proexe/index.html"
+  },
+  {
+    "revision": "4a9e901b38a9fca09202",
+    "url": "/proexe/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "4057d1327495342e8479",
+    "url": "/proexe/static/css/main.03f57d57.chunk.css"
+  },
+  {
+    "revision": "4a9e901b38a9fca09202",
+    "url": "/proexe/static/js/2.13490d6e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/proexe/static/js/2.13490d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4057d1327495342e8479",
+    "url": "/proexe/static/js/main.971d7e0e.chunk.js"
+  },
+  {
+    "revision": "c3f194283fb741e9f005",
+    "url": "/proexe/static/js/runtime-main.accc570c.js"
+  }
+]);
